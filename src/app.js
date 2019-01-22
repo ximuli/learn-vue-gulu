53524f76b96cfaca89e189994ac92542080625a3
ximuli <ximuli666@163.com>
2019-01-22 20:14:43 +0800

    创建 button-group 组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './buttonGroup'
 
 Vue.component('l-button', Button)
 Vue.component('l-icon', Icon)
+Vue.component('l-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/buttonGroup.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="l-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+  .l-button-group { display: inline-flex; vertical-align: middle;
+    > .l-button {
+      border-radius: 0; margin-left: -1px;
+      &:hover { position: relative; z-index: 1;}
+      &:first-child {
+        border-bottom-left-radius: var(--border-radius);
+        border-top-left-radius: var(--border-radius);
+      }
+      &:last-child {
+        border-bottom-right-radius: var(--border-radius);
+        border-top-right-radius: var(--border-radius);
+      }
+    }
+  }
+</style>
