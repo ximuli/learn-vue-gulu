cecd08353da313b5b5c535520b303c288f4022e3
ximuli <ximuli666@163.com>
2019-01-22 20:45:52 +0800

    第一个元素不需要 margin-left -1px

--- src/buttonGroup.vue
+++ src/buttonGroup.vue
@@ -20,7 +20,8 @@ export default {
 <style lang="scss" scoped>
   .l-button-group { display: inline-flex; vertical-align: middle;
     > .l-button {
-      border-radius: 0; margin-left: -1px;
+      border-radius: 0;
+      &:not(:first-child) { margin-left: -1px; }
       &:hover { position: relative; z-index: 1;}
       &:first-child {
         border-bottom-left-radius: var(--border-radius);
