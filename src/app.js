3f125d752c716acd83bf3089b7c03b4649f1c41d
ximuli <ximuli666@163.com>
2019-03-27 17:23:07 +0800

    继续开发 grid

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './buttonGroup'
-
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('l-button', Button)
 Vue.component('l-icon', Icon)
 Vue.component('l-button-group', ButtonGroup)
-
 Vue.component('l-input', Input)
+Vue.component('l-row', Row)
+Vue.component('l-col', Col)
 
 new Vue({
   el: '#app',
