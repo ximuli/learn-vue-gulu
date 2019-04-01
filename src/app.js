9091633ea6640b64d3c548a8bcdce86b4b0bdbc4
ximuli <ximuli666@163.com>
2019-04-01 08:56:50 +0800

    创建layout布局相关组件

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ButtonGroup from './buttonGroup'
 import Input from './input'
 import Row from './row'
 import Col from './col'
+import Header from './header'
+import Content from './content'
+import Footer from './footer'
+import Sider from './sider'
+import Layout from './layout'
 
 Vue.component('l-button', Button)
 Vue.component('l-icon', Icon)
@@ -12,6 +17,11 @@ Vue.component('l-button-group', ButtonGroup)
 Vue.component('l-input', Input)
 Vue.component('l-row', Row)
 Vue.component('l-col', Col)
+Vue.component('l-header', Header)
+Vue.component('l-content', Content)
+Vue.component('l-footer', Footer)
+Vue.component('l-sider', Sider)
+Vue.component('l-layout', Layout)
 
 new Vue({
   el: '#app',
---
+++ src/layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="layout">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+
+<style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid red;
+}
+</style>
