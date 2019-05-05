815b6f5c151d9a540a7d4738e180d795194a337b
ximuli <ximuli666@163.com>
2019-05-05 23:05:12 +0800

    添加 eventBus；tabs 切换

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="tabs-head">
     <slot></slot>
-    <slot name="actions"></slot>
+    <div class="actions-wrapper">
+      <slot name="actions"></slot>
+    </div>
   </div>
 </template>
 
 <script>
 	export default {
-		name: "GuluTabsHead"
+		name: "GuluTabsHead",
+    inject: ['eventBus'],
+    created() {
+	   
+    }
 	}
 </script>
 
 <style lang="scss" scoped>
+  $tab-height: 40px;
   .tabs-head {
-  
+    display: flex;
+    height: $tab-height;
+    justify-content: flex-start;
+    align-items: center;
+    border: 1px solid red;
+    > .actions-wrapper {
+      margin-left: auto; // 这个写法可以使元素靠右
+    }
   }
 </style>
--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item">
+  <div class="tabs-item" @click="xxx" :class="classes">
     <slot></slot>
   </div>
 </template>
@@ -7,10 +7,37 @@
 <script>
 	export default {
 		name: "GuluTabsItem",
+    data() {
+			return {
+				active: false
+      }
+    },
     props: {
 			disabled: {
 				type: Boolean,
         default: false
+      },
+      name: {
+				type: String | Number,
+        required: true
+      }
+    },
+    inject: ['eventBus'],
+    computed: {
+			classes() {
+				return {
+					active: this.active
+        }
+      }
+    },
+    created() {
+      this.eventBus.$on('update:selected', (name) => { // 在同一个事件中心上监听此事件
+	      this.active = name === this.name
+      })
+    },
+    methods: {
+			xxx () {
+				this.eventBus.$emit('update:selected', this.name) // 在事件中心 emit 一个事件
       }
     }
 	}
@@ -18,6 +45,10 @@
 
 <style lang="scss" scoped>
   .tabs-item {
-  
+    padding: 0 1em;
+    flex-shrink: 0;
+    &.active {
+      background-color: red;
+    }
   }
 </style>
