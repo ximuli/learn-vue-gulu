eaadfc9e694e92f181ee195972eef4557a8d16dd
ximuli <ximuli666@163.com>
2019-05-06 22:46:36 +0800

    支持 disabled

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item" @click="xxx" :class="classes">
+  <div class="tabs-item" @click="onClick" :class="classes">
     <slot></slot>
   </div>
 </template>
@@ -26,7 +26,8 @@
     computed: {
 			classes() {
 				return {
-					active: this.active
+					active: this.active,
+          disabled: this.disabled
         }
       }
     },
@@ -36,7 +37,8 @@
       })
     },
     methods: {
-			xxx () {
+			onClick () {
+				if(this.disabled) { return }
 				this.eventBus.$emit('update:selected', this.name, this) // 在事件中心 emit 一个事件
       }
     }
@@ -45,11 +47,16 @@
 
 <style lang="scss" scoped>
   $blue: #1890ff;
+  $disabled-text-color: grey;
   .tabs-item {
     padding: 0 1em; flex-shrink: 0; cursor: pointer; display: flex; align-items: center;
     &.active {
       color: $blue;
       font-weight: bold;
+    }
+    &.disabled {
+      color: $disabled-text-color;
+      cursor: not-allowed;
     }
   }
 </style>
