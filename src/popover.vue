ce1dbf3b5dd9794c52ee4fbed58e62f7d5c52132
ximuli <ximuli666@163.com>
2019-05-15 22:06:48 +0800

    解决父容器有overflow:hidden时造成的bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="popover" @click="xxx">
-    <div class="content-wrapper" v-if="visible">
+  <div class="popover" @click.stop="xxx">
+    <div ref="contentWrapper" class="content-wrapper" v-show="visible" @click.stop>
       <slot name="content"></slot>
     </div>
-    <slot></slot> 
+    <span ref="trigger">
+      <slot></slot> 
+    </span>
   </div>
 </template>
 
@@ -15,10 +17,29 @@ export default {
       visible: false
     }
   },
+  mounted() {
+    console.log(this.$refs.trigger.getBoundingClientRect())
+    console.log(this.$refs.contentWrapper)
+  },
   methods: {
     xxx() {
-      console.log(666)
       this.visible = !this.visible
+      if (this.visible === true) {
+        this.$nextTick(() => {
+          document.body.appendChild(this.$refs.contentWrapper)
+          // this.$refs.trigger.getBoundingClientRect()
+          let { width, height, top, bottom, left, right } = this.$refs.trigger.getBoundingClientRect()
+          this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+          this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+          let eventHandler = () => {
+            this.visible = false
+            console.log('删除')
+            document.removeEventListener('click', eventHandler)
+          }
+          console.log('新增')
+          document.addEventListener('click', eventHandler)
+        })
+      }
     }
   }
 }
@@ -27,8 +48,8 @@ export default {
 <style lang="scss" scoped>
 .popover {
   display: inline-block; vertical-align: top; position: relative;
-  .content-wrapper {
-    position: absolute; bottom: 100%; left: 0; border: 1px solid red; box-shadow: 0 0 3px #333;
-  }
+}
+.content-wrapper {
+  position: absolute; border: 1px solid red; box-shadow: 0 0 3px #333; transform: translateY(-100%);
 }
 </style>
