73b310a648bc88ca23d786fc7f8b61440fdad1dd
ximuli <ximuli666@163.com>
2019-05-16 08:35:32 +0800

    重构tabs.vue

--- src/tabs.vue
+++ src/tabs.vue
@@ -31,19 +31,27 @@
 				eventBus: this.eventBus
       }
     },
-    mounted() {
-			if (this.$children.length === 0) {
-				console && console.warn && console.warn('tabs的子组件应该是tabs-head和tabs-body，但是你没有写子组件')
-      }
-			this.$children.forEach(vm => {
-				if (vm.$options.name === 'GuluTabsHead') {
-					vm.$children.forEach(childVm => {
-						if (childVm.$options.name === 'GuluTabsItem' && childVm.name === this.selected) {
-							this.eventBus.$emit('update:selected', this.selected, childVm)
-            }
-          })
+    methods: {
+      checkChildren() {
+        if (this.$children.length === 0) {
+          console && console.warn && console.warn('tabs的子组件应该是tabs-head和tabs-body，但是你没有写子组件')
         }
-      })
+      },
+      selectTab() {
+        this.$children.forEach(vm => {
+          if (vm.$options.name === 'GuluTabsHead') {
+            vm.$children.forEach(childVm => {
+              if (childVm.$options.name === 'GuluTabsItem' && childVm.name === this.selected) {
+                this.eventBus.$emit('update:selected', this.selected, childVm)
+              }
+            })
+          }
+        })
+      }
+    },
+    mounted() {
+			this.checkChildren()
+			this.selectTab()
     }
 	}
 </script>
