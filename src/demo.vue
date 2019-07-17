e413fcd9d0e21cc8bc3dc97c1aab5f72e8b8fcc7
ximuli <ximuli666@163.com>
2019-07-17 08:58:47 +0800

    睡了一觉后 parcel 莫名其妙跑不起来，切换为 Vue CLI 3

---
+++ src/demo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <l-button>你好，世界</l-button>
+  </div>
+</template>
+
+<script>
+import Button from './button'
+
+export default {
+  name: 'demo',
+  components: {
+    'l-button': Button
+  }
+}
+</script>
+
+<style>
+
+</style>
